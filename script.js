4207d136fbfc8c395bdaf092fe248edc836b09b7
uttamboda <167513333+uttamboda@users.noreply.github.com>
2025-01-24 12:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Your code here.
+let isDragging = false;
+let startX, scrollLeft;
+
+const itemsContainer = document.querySelector('.items');
+
+itemsContainer.addEventListener('mousedown', (e) => {
+  isDragging = true;
+  startX = e.pageX - itemsContainer.offsetLeft;
+  scrollLeft = itemsContainer.scrollLeft;
+  itemsContainer.classList.add('active'); 
+});
+
+itemsContainer.addEventListener('mousemove', (e) => {
+  if (!isDragging) return; 
+  
+  const x = e.pageX - itemsContainer.offsetLeft;
+  const scroll = (x - startX) * 2; 
+  itemsContainer.scrollLeft = scrollLeft - scroll;
+});
+
+itemsContainer.addEventListener('mouseup', () => {
+  isDragging = false;
+  itemsContainer.classList.remove('active'); 
+});
+
+itemsContainer.addEventListener('mouseleave', () => {
+  isDragging = false;
+  itemsContainer.classList.remove('active');
+});
